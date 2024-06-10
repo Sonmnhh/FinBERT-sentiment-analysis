c8d7338e0720b941d1f9ccd6ebd05753f658afd7
Sonmnhh <minhson2118@gmail.com>
2024-06-10 17:18:24 +0700

    Add files via upload

---
+++ sentiment-analyser.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[17]:
+
+
+pip install streamlit
+
+
+# In[18]:
+
+
+pip install -U textblob
+
+
+# In[19]:
+
+
+import streamlit as st  
+from textblob import TextBlob 
+
+
+# In[20]:
+
+
+st.title("Sentiment Analysis WebApp.")  
+t = st.text_area("Please Enter your Sentences!") 
+
+
+# In[27]:
+
+
+if st.button("Analyze the Sentiment"):
+    blob = TextBlob(message)
+    result = blob.sentiment
+    plrty = result.polarity
+    subjty = result.subjectivity
+
+    if plrty < 0:
+        st.warning("The given text has negative sentiments associated with it: " + str(plrty))
+
+    elif plrty > 0:
+        st.success("The given text has positive sentiments associated with it: " + str(plrty))
+
+    else:
+        st.info("The given text has neutral sentiments associated with it: " + str(plrty))
+
+
+    st.success(result)  
+
+
+# In[ ]:
+
+
+
+
